c8f81b0e56071a471b4528f0f1c62b732fd08c5c
Bruno Sampayo Mello <bsamps.devops@gmail.com>
2023-01-17 22:17:34 -0300

    Base Criada

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,18 @@
+import { Request,Response } from "express";
+
+export const home = (req:Request, res:Response) =>{
+
+    //Res.render ("pages/page")
+}
+export const dogs = (req:Request, res:Response) =>{
+
+    //Res.render ("pages/page")
+}
+export const cats = (req:Request, res:Response) =>{
+
+    //Res.render ("pages/page")
+}
+export const fishes = (req:Request, res:Response) =>{
+
+    //Res.render ("pages/page")
+}
