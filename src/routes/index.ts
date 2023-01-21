0f1762a6c7f3eb8072dd4be147863ab288a56ee2
Bruno Sampayo Mello <bsamps.devops@gmail.com>
2023-01-21 11:32:32 -0300

    final project v1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,7 +10,7 @@ const router = Router();
 router.get('/' ,PageController.home);
 router.get('/dogs',PageController.dogs)
 router.get('/cats',PageController.cats)
-router.get('/fishes',PageController.dogs)
+router.get('/fishes',PageController.fishes)
 
 router.get("/search", SearchController.search)
 
